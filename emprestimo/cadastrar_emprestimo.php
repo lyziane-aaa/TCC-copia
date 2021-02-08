6c6541d37408a11230f37c1a7f9a0c67002ccac0
lucasbranford <lucas.carlos@academico.ifrn.edu.br>
2021-02-08 17:30:14 -0300

    Commit Lucas - Empréstismos

--- emprestimo/cadastrar_emprestimo.php
+++ emprestimo/cadastrar_emprestimo.php
@@ -31,31 +31,42 @@ sair da sessão clicando no icone de usuário. -->
                 <hr class="divisor">
                 <br>
 
-                <label for="obj_emp">Objetos disponíveis para empréstimo</label>
-                <select name="obj_emp" id="nome-objeto">
-                    <?php
 
-                    $resultado_pat = mysqli_query($conn, "SELECT * FROM patrimonioativo WHERE emprestado_pat = 1 && emprestavel = 0") or die("erro " . mysqli_error($conn));
+                <?php
+
+                $resultado_pat = mysqli_query($conn, "SELECT * FROM patrimonioativo WHERE emprestado_pat = 0 && emprestavel = 1") or die("erro " . mysqli_error($conn));
+                $row_pat = mysqli_fetch_array($resultado_pat);
+              
+                if (isset($row_pat)) {
+                    echo '  <label for="obj_emp">Objetos disponíveis para empréstimo</label>
+                    <select name="obj_emp" id="nome-objeto">';
                     while ($row_pat = mysqli_fetch_array($resultado_pat)) {
                         echo '<option value="' . $row_pat["id_pat"] . '">' . $row_pat["nome_pat"] . '</option>';
                     }
-                    ?>
+                    echo '</select>';
+                } else {
+                    echo '<label for="obj_emp">Objetos disponíveis para empréstimo</label>
+                        <select name="obj_emp" readonly ="readonly" id="nome-objeto">
+                        <option value="">Não há objetos disponíveis para empréstimos</option>
+                        </select>';
+                }
+                ?>
                 </select>
                 <br>
 
                 <label for="nome_emp">Nome completo:</label>
                 <input type="text" name="nome_emp" id="nome-usuarios" required>
-                <br>    
+                <br>
 
                 <label for="matricula_emp">Matrícula ou CPF:</label>
                 <input type="text" name="matricula_emp" required>
-                <br>   
+                <br>
 
                 <label for="gremista_emp">Gremista que emprestou:</label>
-                <input type="text" name="gremista_emp" required readonly value ="<?php echo $_SESSION['nome_usuarios']?>">
-                <br>   
+                <input type="text" name="gremista_emp" required readonly value="<?php echo $_SESSION['nome_usuarios'] ?>">
+                <br>
                 <label for="condicao" id="post">Condição:</label>
-                <select name="condicaoEmp" id="condicao">
+                <select name="condicao_emp" id="condicao">
                     <option value="Novo">Novo</option>
                     <option selected value="Normal">Normal</option>
                     <option value="Desgastado">Desgastado</option>
@@ -63,14 +74,17 @@ sair da sessão clicando no icone de usuário. -->
 
                 <br>
 
-                <label for="data_emp">Gremista que emprestou:</label>
-                <input type="text" name="data_emp" required readonly value ="<?php $data = date('d/m/Y - H:i:s'); echo $data;?>">
-              
+                <label for="data_emp">Data e horário do empréstimo:</label>
+                <input type="text" name="data_emp" required readonly value="<?php $data = date('d/m/Y - H:i:s');
+                                                                            echo $data; ?>">
+                <!-- Input Escondida -->
+                <input type="number" name="emprestado_pat" required readonly value="1" hidden>
+
                 <br>
-                
+
                 <input type="reset" class="botao" value="Limpar">
                 <input type="submit" class="botao" onclick="msg()" value="Cadastrar">
-                
+
             </form>
     <?php
             include_once("../footer.php");
