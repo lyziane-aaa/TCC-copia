2a2d16dd8b797e313720195665c4c7215fe71aee
lucasbranford <lucas.carlos@academico.ifrn.edu.br>
2021-02-08 16:08:31 -0300

    Enter

--- emprestimo/cadastrar_emprestimo.php
+++ emprestimo/cadastrar_emprestimo.php
@@ -1,63 +1,85 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar Empréstimo</title>
-    <!-- Links CSS -->
-    <link rel="stylesheet" href="../css/estilo.css">
-    <!-- SCRIPTS -->
-    <script src="../js/scripts.js"></script>
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+?>
+    <!DOCTYPE html>
+    <html lang="pt-br">
 
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cadastrar Empréstimo</title>
+        <!-- Links CSS -->
+        <link rel="stylesheet" href="../css/estilo.css">
+        <!-- SCRIPTS -->
+        <script src="../js/scripts.js"></script>
 
-<!-- Analisar possibilidade de uma segunda logo e de colocar novamente a o local onde podiamos 
+    </head>
+
+    <!-- Analisar possibilidade de uma segunda logo e de colocar novamente a o local onde podiamos 
 sair da sessão clicando no icone de usuário. -->
-<body class="tema-escuro">
-    <?php 
+
+    <body class="tema-escuro">
+        <?php include_once("../conexao.php");
         include_once("../menu.php");
-        include_once("../conexao.php");
-        if(isset($_SESSION['login']) && isset($_SESSION['nivel'])) {    
-    ?>
-    <form action="inserir_emprestimo.php" class = "cadastro" method="post">
-        <h2 class="cad-titulo"><img src="../imagens/emprestimos.png"> Cadastro Empréstimo</h2>
-        <hr class="divisor"> 
-        <br> 
-        <label for="nome-objeto">Nome do objeto:</label>
-        <input type="text" name="nomeDoObjeto" id="nome-objeto" required>
-        <br>
 
-        <label for="condicao" id="post">Condição:</label>
-        <select name="condicaoEmp" id="condicao">
-            <option value="Novo">Novo</option>
-            <option selected value="Normal">Normal</option>
-            <option selected value="Desgastado">Desgastado</option>
-        </select>
-        <br> 
-        <br> 
-        <?php 
-            $hoje = date('d/m/Y');
+
+        if (isset($_SESSION['login']) && isset($_SESSION['nivel'])) {
         ?>
+            <form action="inserir_emprestimo.php" class="cadastro" method="post">
+                <h2 class="cad-titulo"><img src="../imagens/emprestimos.png"> Cadastro Empréstimo</h2>
+                <hr class="divisor">
+                <br>
 
-        <input type="hidden" name="dataEmp" value="<?php echo $hoje; ?>">
+                <label for="obj_emp">Objetos disponíveis para empréstimo</label>
+                <select name="obj_emp" id="nome-objeto">
+                    <?php
 
-        <label for="detalhes">Detalhes</label> 
-        <textarea name="detalhesEmp" id="detalhes" 
-        cols="10" rows="4" maxlength="800" placeholder="Descreva o Objeto"></textarea>
-        <br>       
+                    $resultado_pat = mysqli_query($conn, "SELECT * FROM patrimonioativo WHERE emprestado_pat = 1 && emprestavel = 0") or die("erro " . mysqli_error($conn));
+                    while ($row_pat = mysqli_fetch_array($resultado_pat)) {
+                        echo '<option value="' . $row_pat["id_pat"] . '">' . $row_pat["nome_pat"] . '</option>';
+                    }
+                    ?>
+                </select>
+                <br>
 
-        <input type="submit" class="botao" onclick="msg()" value="Cadastrar">
-        <input type="reset" class="botao" value="Limpar">
-    </form>
-    <?php 
-    include_once("../footer.php"); 
+                <label for="nome_emp">Nome completo:</label>
+                <input type="text" name="nome_emp" id="nome-usuarios" required>
+                <br>    
+
+                <label for="matricula_emp">Matrícula ou CPF:</label>
+                <input type="text" name="matricula_emp" required>
+                <br>   
+
+                <label for="gremista_emp">Gremista que emprestou:</label>
+                <input type="text" name="gremista_emp" required readonly value ="<?php echo $_SESSION['nome_usuarios']?>">
+                <br>   
+                <label for="condicao" id="post">Condição:</label>
+                <select name="condicaoEmp" id="condicao">
+                    <option value="Novo">Novo</option>
+                    <option selected value="Normal">Normal</option>
+                    <option value="Desgastado">Desgastado</option>
+                </select>
+
+                <br>
+
+                <label for="data_emp">Gremista que emprestou:</label>
+                <input type="text" name="data_emp" required readonly value ="<?php $data = date('d/m/Y - H:i:s'); echo $data;?>">
+              
+                <br>
+                
+                <input type="reset" class="botao" value="Limpar">
+                <input type="submit" class="botao" onclick="msg()" value="Cadastrar">
+                
+            </form>
+    <?php
+            include_once("../footer.php");
         }
-        else{
-        header("location:../usuarios/login.php");	
-        }   
+    } else {
+        header("location:../usuarios/login.php");
+    }
     ?>
 
-</body>
-</html>
+    </body>
 
-
+    </html>
--- patrimonio/inserir_patrimonio.php
+++ patrimonio/inserir_patrimonio.php
@@ -7,12 +7,12 @@ $codBarrasPat = filter_input(INPUT_POST, 'codBarrasPat', FILTER_SANITIZE_STRING)
 $obtencaoPat = filter_input(INPUT_POST, 'obtencaoPat', FILTER_SANITIZE_STRING);
 $custoObt = filter_input(INPUT_POST, 'custoObt', FILTER_SANITIZE_NUMBER_FLOAT);
 $obsPat = filter_input(INPUT_POST, 'obsPat', FILTER_SANITIZE_STRING);
-
+$statusPat = filter_input(INPUT_POST, 'statusPat', FILTER_SANITIZE_STRING);
 
 $resultado_insert = "INSERT INTO patrimonioativo (nomePat, codBarrasPat, obtencaoPat, custoObt,obsPat,dataCadPat) 
-VALUES ('$nomePat', '$codBarrasPat', '$obtencaoPat', '$custoObt', '$obsPat',NOW())";
+VALUES ('$nomePat', '$codBarrasPat', '$obtencaoPat', '$custoObt', '$obsPat','$statusPat',NOW())";
 
 $resultado_insertPat = mysqli_query($conn, $resultado_insert);
 
-header('location: cadastroPatrimonio.php');
+header('location: cadastrar_Patrimonio.php');
 ?>
