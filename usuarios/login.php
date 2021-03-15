8f4e673059026ccfd6da67b9bec57dcf3a7d5d2d
Eduardo-M-P <eduardo.paiva@escolar.ifrn.edu.br>
2021-03-15 18:05:02 -0300

    Novo login 90%

--- usuarios/login.php
+++ usuarios/login.php
@@ -11,9 +11,82 @@ if (!isset($_SESSION['login']) && !isset($_SESSION['senha'])){
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="/TCC/_css/estilo.css">
-	<link rel="stylesheet" type="text/css" href="/TCC/_css/login.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" href="/TCC/_css/bootstrap/css/bootstrap.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+<style>
+	.container{
+		display: grid;
+		grid-template-columns: 1fr 3fr;
+		grid-template-columns: 30vh 70vh;
+		grid-template-rows: 6vh ;
+		grid-template-areas: 
+		"h h"
+		"a m";
+		height: 500px;
+		border-style: solid 1px gray;
+		margin-top: 8%;
+		margin-left: 25%;
+		background-color: whitesmoke;
+		width: min-content;
+		padding: 0;
+		border-radius: 20px;
+		font-family: 'nasalization';
+	}
+
+	.container hr{
+		color:white;
+		margin-top: 0;
+		margin-right: 10px;
+	}
+	main{
+		grid-area: m;
+		margin-left: 30px;
+		margin-top: 20px;
+		text-align: center;
+	}
+
+	aside{
+		grid-area: a;
+		border-right: solid 2px black;
+		background-image: url("../imagens/aside-login.jpg");
+		background-size: cover;
+		background-repeat: no-repeat;
+		border-bottom-left-radius: 20px;
+	}
+	.modal-header{
+		grid-area: h;
+		border-top-left-radius:20px;
+		border-top-right-radius:20px;
+	}
+
+	.form-control{
+		width:  80% !important;
+		flex:unset !important;
+		margin-bottom: 35px !important;
+	}
+
+	#user, #password{
+		margin-bottom: 35px !important;
+	}
+	body{
+		background-image: url("../imagens/fundo_login.jpg");
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+
+	.btn-lg{
+		width: 50%;
+		color:white ;
+	}
+
+	h5{
+		color: white;
+		font-family: 'nasalization';
+		padding:0;
+	}
+</style>
 
 <script type=" text/javascript">
 	setTimeout(function(){
@@ -23,6 +96,7 @@ if (!isset($_SESSION['login']) && !isset($_SESSION['senha'])){
 	}
 	}, 5000);
 	</script>
+
 	<script src="/TCC/js/scripts.js"></script>
 </head>
 <?php
@@ -71,26 +145,47 @@ if ((isset($_POST['login'])) && (isset($_POST['senha']))) {
 	}
 }
 ?>
+<body>
 
-<div class="corpo">
-	<form action="" method="post">
+<div class="container">
+	<div class="modal-header bg-danger"> 
+		<h5>Aplicativo de Gerenciamento ELLY</h5>
+	</div>
+	<aside>
+	</aside>
+
+	<main>
+		<form method="$_POST" action="">
 		<h2>Área de Login</h2>
-		<div class="inputWithIcon">
-			<input type="text" placeholder="Usuário" name="login" required="true">
-			<i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
+		<hr>
+		<br>
+		<div class="input-group mb-3">
+			<button class="btn btn-outline-info btn-sm" id="user">
+				<span class="material-icons">
+					person
+				</span>
+			</button>
+			<input type="text" name="login" class="form-control form-control-lg" placeholder="Nome de Usuário" aria-describedby="user" required>
+			<br>
 		</div>
-
-		<div class="inputWithIcon">
-			<input type="password" placeholder="Senha" name="senha" required="true">
-			<i class="fa fa-lock fa-lg fa-fw" aria-hidden="true"></i>
+		<div class="input-group mb-3">
+			<button class="btn btn-outline-info btn-sm" id="password">
+				<span class="material-icons">
+					lock
+				</span>
+			</button>
+			<input type="password" name="senha" class="form-control form-control-lg" aria-describedby="password" placeholder="Senha"  required>
+			<br>
 		</div>
-
-		<input type="submit" value="Logar">
-		<input type="reset" value="Resetar">
-
-		<?= $mensg1 ?>
-	</form>
+		<button class="btn btn-lg bg-danger"> Entrar</button>
+		
+		</form>
+	</main>
 </div>
+
+
+
+
 <?php
 } else { 
 	//redireciona para o início caso esteja logado
